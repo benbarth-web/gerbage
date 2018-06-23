991cd21c493d832144f57a8f0ea6c0d22e421660
Ben Barth <benbarth@users.noreply.github.com>
2018-06-23 09:46:06 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function() {
 	$("random-gif").attr("src", "/img/loading.gif");
-	$.ajax("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=garbage", {
+	$.ajax("https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=garbage", {
 		success: function(data, textStatus, jqXHR) {
 			$("#random-gif").attr("src", data.data.image_original_url);
 		},
