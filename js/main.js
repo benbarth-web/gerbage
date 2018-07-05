5bf961aea5eae27a82a0c8ed2028b3f6d58e0b62
Ben Barth <benbarth@users.noreply.github.com>
2018-07-04 20:51:30 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 $(function() {
-	$("random-gif").attr("src", "/img/loading.gif");
-	$.ajax("https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=garbage", {
-		success: function(data, textStatus, jqXHR) {
-			$("#random-gif").attr("src", data.data.image_original_url);
-		},
-		error: function() {
-			$("#random-gif").attr("src", "/img/gerbage.gif");
-		}
-	})
+    $('#hash').text(window.location.hash.substring(1));
+    $("random-gif").attr("src", "/img/loading.gif");
+    $.ajax("https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=garbage", {
+        success: function(data, textStatus, jqXHR) {
+            $("#random-gif").attr("src", data.data.image_original_url);
+        },
+        error: function() {
+            $("#random-gif").attr("src", "/img/gerbage.gif");
+        }
+    })
 })
