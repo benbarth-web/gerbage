087221ddc5de5dc1193528601a28b1bec6577cce
Ben Barth <barthben@gmail.com>
2019-03-02 22:13:02 -0500

    Include image with share url

--- js/main.js
+++ js/main.js
@@ -1,22 +1,37 @@
 $(function () {
+
+	baseUrl = function () { return window.location.href.replace(window.location.hash, ''); }
+
 	var hash = window.location.hash.substring(1);
+
+	if (hash && window.location.href == localStorage.getItem("lastKnownHref")) {
+		location = baseUrl();
+	}
+	localStorage.setItem("lastKnownHref", window.location.href);
+
 	var obj;
 	if (hash) {
 		var hashDecoded = decodeURIComponent(atob(hash));
 		obj = JSON.parse(hashDecoded);
 	}
+
 	if (obj && obj.message) {
 		$('#message').text(obj.message);
 	}
-	$("random-gif").attr("src", "/img/loading.gif");
-	$.ajax("https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=garbage", {
-		success: function (data, textStatus, jqXHR) {
-			$("#random-gif").attr("src", data.data.image_original_url);
-		},
-		error: function () {
-			$("#random-gif").attr("src", "/img/gerbage.gif");
-		}
-	})
+
+	if (obj && obj.image) {
+		$("#random-gif").attr("src", obj.image);
+	} else {
+		$("random-gif").attr("src", "/img/loading.gif");
+		$.ajax("https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=garbage", {
+			success: function (data, textStatus, jqXHR) {
+				$("#random-gif").attr("src", data.data.image_original_url);
+			},
+			error: function () {
+				$("#random-gif").attr("src", "/img/gerbage.gif");
+			}
+		})
+	}
 
 	if (obj && obj.redirectUrl && obj.redirectTimeout) {
 		window.setTimeout(function () {
@@ -24,24 +39,22 @@ $(function () {
 		}, obj.redirectTimeout);
 	}
 
-	share = function () {
-		var message = prompt("Message to display:");
-		var redirectUrl = prompt("Redirect to URL (Optional):");
-		var redirectTimeout;
-		if (redirectUrl) {
-			redirectTimeout = prompt("Number of seconds until redirect:", 10);
-		}
-		redirectTimeout *= 1000;
+	createShareUrl = function (message, image, redirectUrl, redirectTimeout) {
+		var newObj = { message, image, redirectUrl, redirectTimeout }
+		return baseUrl() + "#" + btoa(encodeURIComponent(JSON.stringify(newObj)));
+	}
 
-		var newObj = { message, redirectUrl, redirectTimeout }
-		var baseUrl = window.location.href.replace(window.location.hash, '');
-		var newUrl = baseUrl + "#" + btoa(encodeURIComponent(JSON.stringify(newObj)));
+	share = function () {
+		var message = prompt("Include an optional message:");
+		var image = $("#random-gif").attr("src");
+
+		var newUrl = createShareUrl(message, image, null, null);
 
 		if (typeof copyTextToClipboard === "function") {
 			alert("The URL has been copied to the clipboard. " + newUrl)
 			copyTextToClipboard(newUrl);
-		} else {
-			window.location.href = newUrl;
 		}
+		window.location.href = newUrl;
 	}
+
 })
